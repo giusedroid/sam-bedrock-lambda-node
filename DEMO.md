51e082a156f388c7824232cb27bcd53d99649976
gbatt <gbatt@amazon.com>
2023-10-17 14:05:17 +0100

    init

---
+++ DEMO.md
@@ -0,0 +1,12 @@
+# Demo Flow!
+
+1. Open Cloud9 Editor
+1. clone this repo
+1. `sam build`
+1. `sam deploy -g`
+1. show the code and comments
+1. show bedrock console
+1. get stack name 
+1. get function name with `sam list stack-outputs --stack-name <stack-name>`
+1. `./invoke.sh <function-name> <event-number>`
+1. PROFIT!
---
+++ invoke.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FUNCTION_NAME=$1
+EVENT_NUMBER=$2
+
+echo "Invoking function $FUNCTION_NAME"
+aws lambda invoke \
+    --region us-west-2 \
+    --function-name $FUNCTION_NAME \
+    --cli-binary-format raw-in-base64-out \
+    --payload file://events/event-${EVENT_NUMBER}.json \
+    response.json
+
+echo "Response:"
+
+# pretty printing the response
+node -e "console.log(JSON.parse(require('./response').body))"
