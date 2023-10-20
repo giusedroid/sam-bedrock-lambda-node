d5732e9e775c1761ae52f64a1b8a555077249ddc
Giuseppe <giusedroid@users.noreply.github.com>
2023-10-20 13:56:11 +0100

    Update invoke.sh - use region from env

--- invoke.sh
+++ invoke.sh
@@ -5,7 +5,7 @@ EVENT_NUMBER=$2
 
 echo "Invoking function $FUNCTION_NAME"
 aws lambda invoke \
-    --region us-west-2 \
+    --region $AWS_REGION \
     --function-name $FUNCTION_NAME \
     --cli-binary-format raw-in-base64-out \
     --payload file://events/event-${EVENT_NUMBER}.json \
